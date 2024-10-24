b0564c84d06b86dbf11adc5f0bdccb0a54b00395
Cristian Andres Bacca Uribe <andresbacca88@gmail.com>
2024-10-23 23:00:31 -0500

    Mi Primer Programa

---
+++ HolaMundo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * HolaMundo
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("HolaMundo!");
+        System.out.printf("Aqui va una cadena: %s%n",...args:"cristian");
+        System.out.printf("Aqui va un entero :%n%n",..args:10);
+        
+
+    }
+
+
+
+}
